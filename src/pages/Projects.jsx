6125c2066d3fc73324b1f0c95c056627ffd9eb39
david21rios <121947579+david21rios@users.noreply.github.com>
2024-11-29 14:55:39 +0100

    Diseño interfaz projects
    Se actualizo el diseño del componente Projects

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -34,6 +34,7 @@ const GameComponent = () => {
     const [bullets, setBullets] = useState([]); // Estado para las balas disparadas
     const [revealedProjects, setRevealedProjects] = useState([]); // Estado para los proyectos revelados
     const [projects] = useState(generateProjects()); // Estado para los proyectos generados
+    const [color, setColor] = useState('white'); //Estado inicial del color
 
     // Función para disparar una bala
     const shootBullet = useCallback(() => {
@@ -103,6 +104,14 @@ const GameComponent = () => {
         }
     }, [revealedProjects, projects.length]);
 
+    // Efecto para cambiar el color del título de forma aleatoria 
+    useEffect(() => {
+        const colors = ['#9A00FF', '#00AFFF', '#FFDD00', '#6A00FF']; const interval = setInterval(() => {
+            const randomColor = colors[Math.floor(Math.random() * colors.length)];
+            setColor(randomColor); // Cambia a un color aleatorio 
+        }, 2000);
+        return () => clearInterval(interval);
+    }, []);
 
     return (
         <div className="projects-wrapper">
@@ -117,9 +126,7 @@ const GameComponent = () => {
                     {revealedProjects.includes(index) ? (
                         <>
                             <div className={`target-point color${(index % 4) + 1}`} style={{ display: 'none' }}></div>
-                            <div>
-                                <section id="services" className="services"> <div className="container" data-aos="fade-up"> <div className="section-title"> <h2>{project.name}</h2> <p>{project.description}</p> </div> <div className="row"> <div className="col-lg-4 col-md-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100"> <div className={project.icon} style={{ cursor: 'pointer' }}> <div className="icon"> <svg width="100" height="100" viewBox="0 0 600 600" xmlns="http://www.w3.org/2000/svg"> <path stroke="none" strokeWidth="0" fill="#f5f5f5" d="M300,521.0016835830174C376.1290562159157,517.8887921683347,466.0731472004068,529.7835943286574,510.70327084640275,468.03025145048787C554.3714126377745,407.6079735673963,508.03601936045806,328.9844924480964,491.2728898941984,256.3432110539036C474.5976632858925,184.082847569629,479.9380746630129,96.60480741107993,416.23090153303,58.64404602377083C348.86323505073057,18.502131276798302,261.93793281208167,40.57373210992963,193.5410806939664,78.93577620505333C130.42746243093433,114.334589627462,98.30271207620316,179.96522072025542,76.75703585869454,249.04625023123273C51.97151888228291,328.5150500222984,13.704378332031375,421.85034740162234,66.52175969318436,486.19268352777647C119.04800174914682,550.1803526380478,217.28368757567262,524.383925680826,300,521.0016835830174"></path> </svg> <i className={project.i}></i> </div> <h4><a href={project.url} style={{ color: 'blue', textDecoration: 'underline' }}>{project.name}</a></h4></div> </div> </div> </div> </section>
-                            </div>
+                            {/* Iconos con bootstrap */} <div> <section id="services" className="services"> <div className="container" data-aos="fade-up"> <div className="section-title"> <h2 style={{ color }}>{project.name}</h2> <p>{project.description}</p> </div> <div className="row"> <div className="col-lg-4 col-md-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100"> <div className={`project-icon ${project.icon}`} style={{ color: `${project.color}` }} onClick={() => window.open(project.url, '_blank')} > <div className="icon"> <i className={project.i}></i> </div> <h4 style={{ display: 'none' }}>{project.name}</h4></div> </div> </div> </div> </section> </div>
                         </>
                     ) : (
                         <div className={`target-point color${(index % 4) + 1}`}></div>
--- src/utils/generateProjects.js
+++ src/utils/generateProjects.js
@@ -4,7 +4,7 @@ const generateProjects = () => {
         { id: 1, name: "Fundacion Manantial", image: "/assets/images/project1.jpg", url: "https://funmanantial.org/", i: "bi bi-droplet-half", icon: "icon-box iconbox-blue" },
         { id: 2, name: "Rivaltec LTDA", image: "/assets/images/project2.jpg", url: "https://www.rivaltec.net/", i: "bi bi-cpu-fill", icon: "icon-box iconbox-orange" },
         { id: 3, name: "Los Mexikas", image: "/assets/images/project3.jpg", url: "https://www.losmexikas.com/", i: "bi bi-egg-fried", icon: "icon-box iconbox-pink" },
-        { id: 4, name: "David Rios", image: "/assets/images/project4.jpg", url: "https://www.davidriosl.com/", i: "bx bx-file", icon: "icon-box iconbox-orange" },
+        { id: 4, name: "David Rios", image: "/assets/images/project4.jpg", url: "https://www.davidriosl.com/", i: "bi bi-file-earmark", icon: "icon-box iconbox-orange" },
         // { id: 5, name: "Project 5", image: "/assets/images/project5.jpg", url: "https://example.com/project5" },
         // { id: 6, name: "Project 6", image: "/assets/images/project6.jpg", url: "https://example.com/project6" },
         // { id: 7, name: "Project 7", image: "/assets/images/project7.jpg", url: "https://example.com/project7" },
